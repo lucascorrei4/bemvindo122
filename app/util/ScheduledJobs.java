572de0b74f80ee7dd871b6b6158ae40d4ae58f48
Lucas Correia <lucascorreiaevangelista@gmail.com>
2017-09-05 02:32:06 -0300

    sales funnel

---
+++ app/util/ScheduledJobs.java
@@ -0,0 +1,86 @@
+package util;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import controllers.MailController;
+import controllers.SequenceMailController;
+import models.BodyMail;
+import models.MailList;
+import models.Parameter;
+import models.SendTo;
+import models.Sender;
+import models.SequenceMailQueue;
+import models.StatusMail;
+import play.jobs.Job;
+import play.jobs.On;
+
+//** Fire at 8am (noon) every day **/ 
+//@On("0 0 8 * * ?")
+@On("0 20 2 ? * *")
+public class ScheduledJobs extends Job {
+
+	public void doJob() {
+		verifyIfLeadIsNotInSalesFunnel();
+		sendMailToLead();
+	}
+
+	private void verifyIfLeadIsNotInSalesFunnel() {
+		List<MailList> mailList = new MailList().find("isActive = true order by postedAt desc").fetch();
+		for (MailList mL : mailList) {
+			SequenceMailController.addLeadToSalesFunnel(mL);
+		}
+	}
+
+	public void sendMailToLead() {
+		Calendar calendar = Utils.getBrazilCalendar();
+		calendar.set(Calendar.HOUR, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		List<SequenceMailQueue> sequenceMailQueueList = SequenceMailQueue.find("date(jobDate) = CURRENT_DATE and sent = false").fetch();
+		for (SequenceMailQueue sequenceMailQueue : sequenceMailQueueList) {
+			sendEmailToLead(sequenceMailQueue);
+			sequenceMailQueue.setSent(true);
+			sequenceMailQueue.willBeSaved = true;
+			sequenceMailQueue.merge();
+		}
+	}
+
+	private static void sendEmailToLead(SequenceMailQueue sequenceMailQueue) {
+		if (!Utils.isNullOrEmpty(sequenceMailQueue.getMail()) && Utils.validateEmail(sequenceMailQueue.getMail())) {
+			Parameter parameter = Parameter.all().first();
+			MailController mailController = new MailController();
+			/* SendTo object */
+			SendTo sendTo = new SendTo();
+			sendTo.setDestination(sequenceMailQueue.getMail());
+			sendTo.setName(sequenceMailQueue.getName());
+			sendTo.setSex("");
+			sendTo.setStatus(new StatusMail());
+			/* Sender object */
+			Sender sender = new Sender();
+			sender.setCompany("Seu Pedido Online");
+			sender.setFrom("contato@seupedido.online");
+			sender.setKey("Sales Funnel");
+			/* SendTo object */
+			BodyMail bodyMail = new BodyMail();
+			bodyMail.setTitle1("");
+			bodyMail.setTitle2("");
+			bodyMail.setFooter1("");
+			bodyMail.setImage1(parameter.getLogoUrl());
+			String firstName = sequenceMailQueue.getName().indexOf(" ") > -1 ? sequenceMailQueue.getName().substring(0, sequenceMailQueue.getName().indexOf(" ")) : sequenceMailQueue.getName();
+			bodyMail.setBodyHTML(sequenceMailQueue.getSequenceMail().getDescription().replace("@lead@", firstName));
+			String subject = sequenceMailQueue.getSequenceMail().getTitle().replace("@lead@", firstName);
+			if (mailController.sendHTMLMail(sendTo, sender, bodyMail, subject)) {
+				mailController.sendMailToMeWithCustomInfo("E-mail disparado ao lead!", "Nome: " + sequenceMailQueue.getName() + " - E-mail: " + sequenceMailQueue.getMail());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String name = "Lucas Correia";
+		System.out.println(name.indexOf(" ") > -1 ? name.substring(0, name.indexOf(" ")) : name);
+	}
+}
